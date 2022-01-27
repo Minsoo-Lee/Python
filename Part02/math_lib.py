80ed27dd68033b8ce37bc312fdac449489f72b61
Minsoo-Lee <minsoo.james1101@gmail.com>
2022-01-27 19:48:37 +0900

    01.27

---
+++ Part02/math_lib.py
@@ -0,0 +1,20 @@
+# 절대값 구하기
+print(abs(-77));
+print(abs(77));
+
+# 반올림하기
+print(round(1.222));
+print(round(1.522));
+
+# 최대값 구하기
+print(max(10, -20, 100, 9999));
+# 최소값 구하기
+print(min(10, -20, 100, 9999));
+
+# math 라이브러리의 모든 함수를 사용
+from math import * 
+
+# 제곱근 구하기
+print(sqrt(4.0));
+# 제곱(지수) 구하기, 연산자 **와 동일
+print(pow(3, 2));
